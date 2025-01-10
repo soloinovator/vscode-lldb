0de53137ed5f654a3ace618ac0f7f82a0ece6cf3
Vadim Chugunov <vadimcn@gmail.com>
2025-01-10 12:19:55 -0800

    WIP

--- .github/workflows/analyze_issue.py
+++ .github/workflows/analyze_issue.py
@@ -118,6 +118,8 @@ class IssueAnalyzer:
         f = io.StringIO()
         f.write(f'### Title: {issue["title"]}\n')
         f.write(f'### Author: {issue["user"]["login"]}\n')
+        f.write(f'### State: {issue["state"]}\n')
+        f.write(f'### Labels: {','.join(label["name"] for label in issue["labels"])}\n')
         f.write(f'\n{issue["body"]}\n')
 
         if fetch_comments:
